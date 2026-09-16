96385ee4ee341bd246d926a75d62d5d770bdfbc5
Soster Gabriele <g.soster.4166@vallauri.edu>
2026-09-16 11:15:47 +0200

    Creazione dinamica bottoni

--- Calculator-Soster/Form1.cs
+++ Calculator-Soster/Form1.cs
@@ -10,11 +10,53 @@ using System.Windows.Forms;
 
 namespace Calculator_Soster
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+
+        private char[,] buttons =
+        {
+            {'%', '\u0152', 'C', '\u232B' },
+            {'\u215F', '\u00B2', '\u221A', '\u00F7' },
+            {'7', '8', '9', 'x' },
+            {'4', '5', '6', '-' },
+            {'1', '2', '3', '+' },
+            {'\u00B1', '0', ',', '=' },
+        };
+        public FrmMain()
         {
             InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            MakeButtons();
+        }
+
+        private void MakeButtons()
+        {
+            int btnWidth = 80;
+            int btnHeigth = 60;
+            int posY = 106;
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                int posx = 0;
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+                    Button btn = new Button();
+                    btn.Width = btnWidth;
+                    btn.Height = btnHeigth;
+                    btn.Top = posY;
+                    btn.Left = posx;
+                    btn.Font = new Font("Segoe UI",16);
+                    btn.Text = buttons[i, j].ToString();
+                    Controls.Add(btn);
+                    posx += btnWidth;
+                    
+                }
+                posY += btnHeigth;
+
+            }
+                    
         }
     }
 }
